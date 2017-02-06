f79c5ab6245d4c5636898454539d2695deea19bf
Zhitong Ye <zy924@nyu.edu>
2017-02-06 01:09:20 -0500

    Add, calculate price for bey change

---
+++ bey_change.py
@@ -0,0 +1,6 @@
+def bey_change(price,dur,bp):
+	price_plus = price + price*(-dur)*bp/10000
+	price_minus = price - price*(-dur)*bp/10000
+	return [price_plus,price_minus]
+
+print bey_change(98,4.964,50)
